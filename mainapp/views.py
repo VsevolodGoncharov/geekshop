4eeea2453161e456e4d4509a0a341b6d461c8dec
vsevolod_goncharov <gyro7931@gmail.com>
2021-02-27 19:31:26 +0300

    lesson2

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
+import json
+import os
+
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'geekshop',
+    }
+    return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    file = os.path.join(os.getcwd(), 'mainapp', 'fixtures', 'products.json')
+    with open(file, encoding='utf-8') as f:
+        products_list = json.load(f)
+    context = {
+        'title': 'geekshop - Каталог товаров',
+        'products': products_list,
+    }
+    return render(request, 'mainapp/products.html', context)
