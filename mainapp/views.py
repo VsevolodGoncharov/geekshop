6a380c4c95483814e2a450e7b8e988fdd57ff8c4
vsevolod_goncharov <gyro7931@gmail.com>
2021-03-04 14:48:59 +0300

    change templates for models change for media

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-import json
 import os
+from mainapp.models import Product, ProductCategory
 
 def index(request):
     context = {
@@ -10,9 +10,9 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    products_file = os.path.join(os.getcwd(), 'mainapp', 'fixtures', 'products.json')
     context = {
         'title': 'geekshop - Каталог товаров',
-        'products': json.load(open(products_file, encoding='utf-8')),
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
     return render(request, 'mainapp/products.html', context)
