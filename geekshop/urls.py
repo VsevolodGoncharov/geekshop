7540226ffb16c177b49bcc0479c48c29edf997bb
vsevolod_goncharov <gyro7931@gmail.com>
2021-03-08 18:15:43 +0300

    added authapp

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -23,7 +23,8 @@ from mainapp.views import index, products
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', include('mainapp.urls', namespace='products'))
+    path('products/', include('mainapp.urls', namespace='products')),
+    path('auth/', include('authapp.urls', namespace='auth')),
 ]
 
 if settings.DEBUG:
